29a65b314fe2073789504398a14cf6b5aeb6f20b
DropADraco <dadan090@learner.dvusd.org>
2024-01-23 09:34:14 -0700

    keu

--- Pokedex.true/Pokedex.true/Form1.cs
+++ Pokedex.true/Pokedex.true/Form1.cs
@@ -33,7 +33,28 @@ namespace Pokedex.@true
         {
             InitializeComponent();
         }
+        private Pokemon ReadPokemon(string s)
+        {
+            Pokemon p = new Pokemon();
+            string[] fields = s.Split('|');
+            p.Name = fields[0];
+            p.Type = fields[1];
+            p.Level = int.Parse(fields[2]);
+            p.Atk = int.Parse( fields[3]);
+            p.HP = int.Parse(fields[4]);
+            p.Exp = int.Parse(fields[5]);
+            if (fields[6] == "True")
+                p.Legendary = true;
+            else
+                p.Legendary = false;
+            if (fields[7] == "True")
+                p.Shiny = true;
+            else
+                p.Shiny = false;
+            p.Gen = int.Parse(fields[8]);
 
+            return p;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
